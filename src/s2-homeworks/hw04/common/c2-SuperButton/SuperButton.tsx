fcd52ab382ffa52f96009c6781ad07e397b70589
feddorovich <feddorovich@outlook.com>
2023-02-20 12:11:30 +0300

    hw 4 done without css

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,10 +17,8 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        + ' ' + (disabled ? s.disabled : '')
-        + ' ' + (xType === 'red' ? s.red : xType === 'secondary' ?  s.secondary : '')
-        +  " " + (className ? className : '') // задачка на смешивание классов
+    const finalClassName = `${s.button} ${disabled ? s.disabled : ''} ${xType === 'red' ? s.red : xType === 'secondary' ?  s.secondary : s.default} ${className ? className : ''}`
+
 
     return (
         <button
